41c81d3bbde90bae25ef821254a64ef184bdffd4
Anna Khaitovich <akhaitovich@gmail.com>
2018-12-15 21:26:05 +0100

    Settings.py + respond to messages

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 import os
+import vk
 from flask import Flask, request, json, Response
 from flask_api import FlaskAPI, status
 from settings import * 
@@ -8,7 +9,6 @@ app = FlaskAPI(__name__)
 @app.route('/callback', methods=['POST'])
 def callback():
 	data = request.get_json()
-	print(data)
 	if 'type' in data and data['type'] == 'confirmation':
 		if 'group_id' in data:
 			if data['group_id'] == GROUP_ID:
@@ -19,7 +19,19 @@ def callback():
 			return 'Provide group_id', status.HTTP_401_UNAUTHORIZED
 	return 'Bad request', status.HTTP_400_BAD_REQUEST 
 
+@app.route('/new_message', methods=['POST'])
+def new_message():
+	data = request.get_json()
+	if 'type' in data and data['type'] == 'message_new':
+		session = vk.Session()
+		api = vk.API(session, v=VK_API_VERSION)
+		if 'object' in data and 'user_id' in data['object']:
+			user_id = data['object']['user_id']
+		else:
+			return 'Bad request', status.HTTP_400_BAD_REQUEST
+		api.messages.send(access_token=TOKEN, user_id=str(user_id), message='Sasat+lezhat')
+		return 'ok'
+
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', PORT))
 	app.run(host='0.0.0.0', port=port)
-
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 GROUP_ID = 147073760  # https://vk.com/kak_zaplatit_za_musor
 CONFIRM_TOKEN = '48eae3b0'
 PORT = 8080
+VK_API_VERSION = '5.50'
