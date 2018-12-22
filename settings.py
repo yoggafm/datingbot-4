30c87a3918e28c53cdf624c9005d87e6c29db192
Anna Khaitovich <akhaitovich@gmail.com>
2018-12-22 20:13:02 +0100

    Parameterize settings.py

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 FLASK_DEBUG = os.environ.get('FLASK_DEBUG', False)
 TOKEN = os.environ.get('TOKEN', '')
-GROUP_ID = 147073760  # https://vk.com/kak_zaplatit_za_musor
-CONFIRM_TOKEN = '48eae3b0'
-PORT = 8080
-VK_API_VERSION = '5.50'
-QNA_FILE = 'qna.json'
+GROUP_ID = os.environ.get('GROUP_ID', '')
+CONFIRM_TOKEN = = os.environ.get('CONFIRM_TOKEN', '')
+PORT = os.environ.get('PORT', 8080)
+VK_API_VERSION = os.environ.get('VK_API_VERSION', '5.50')
+QNA_FILE = os.environ.get('QNA_FILE', 'qna.json')
